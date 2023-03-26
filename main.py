ec19629334466e80b1066d3a0f605efd8c89686b
演变 <cnlimiter@163.com>
2023-03-26 22:42:33 +0800

    + initial

---
+++ main.py
@@ -0,0 +1,15 @@
+import uvicorn
+
+from bootstrap.application import create_app
+from config.config import settings as app_config
+
+app = create_app()
+
+
+@app.get("/")
+async def root():
+    return "Welcome!"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host=app_config.SERVER_HOST, port=app_config.SERVER_PORT)
